e894378dd2347262a076a011bb51ec01e33fefb2
Roc <1608976833@qq.com>
2023-01-29 13:58:48 +0800

    refactor: 简化导航

--- Demo3/Shared/Header.razor.cs
+++ Demo3/Shared/Header.razor.cs
@@ -4,18 +4,34 @@ namespace Demo3.Shared
 {
     public partial class Header
     {
+        private string themeColor = "background-color:#121212;color:#a9a9b3";
         private string currentTheme = "Light";
 
         protected override async Task OnInitializedAsync()
         {
             currentTheme = await Common.GetStorageAsync("theme") ?? "Light";
-
+            if (currentTheme == "Dark")
+            {
+                themeColor = "background-color:#121212;color:#a9a9b3";
+            }
+            else
+            {
+                themeColor = "";
+            }
             await Common.InvokeAsync("window.func.switchTheme");
         }
 
         private async Task SwitchTheme()
         {
             currentTheme = currentTheme == "Light" ? "Dark" : "Light";
+            if (currentTheme == "Dark")
+            {
+                themeColor = "background-color:#121212;color:#a9a9b3";
+            }
+            else
+            {
+                themeColor = "";
+            }
 
             await Common.SetStorageAsync("theme", currentTheme);
 
